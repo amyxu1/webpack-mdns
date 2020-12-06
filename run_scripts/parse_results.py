241c12874feff5f8c51ec27d9a9c736f09413c32
Amy Xu <32859554+amyxu1@users.noreply.github.com>
2020-12-05 20:48:46 -0800

    Create parse_results.py

---
+++ run_scripts/parse_results.py
@@ -0,0 +1,26 @@
+def main(argv):
+	if len(argv) < 3:
+		print('usage: python parse_results.py [total result file name] [names of results files]')
+		sys.exit(2)
+
+	results_files = argv[2:]
+	result = open(argv[1], 'w+')
+
+	for result_file in results_files:
+		with open(result_file) as f:
+			lines = f.readlines()
+		in_result = False
+
+		for line in lines:
+			if not in_result and line.startswith('>>>>'):
+				in_result = True 
+				continue
+
+			if in_result:
+				if line.startswith('<<<<'):
+					in_result = False
+					continue
+				# may want to update in the future
+				result.write(line + '\n')
+
+	result.close()
